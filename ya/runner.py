7962e891af7b214c0dd6ba6da4e69f3134dadcd6
heerozh <heeroz@gmail.com>
2026-01-29 18:53:46 +0800

    打印堆栈

--- ya/runner.py
+++ ya/runner.py
@@ -130,7 +130,7 @@ async def run_single_executor(
             rtn = await benchmark_func(*fixture_results)
         except Exception as e:
             print(f"Error in benchmark '{benchmark_name}': {e}， thread exited.")
-            break
+            raise
 
         # Calculate execution time
         execution_time = (time.time() - call_start) * 1000.0  # in milliseconds
